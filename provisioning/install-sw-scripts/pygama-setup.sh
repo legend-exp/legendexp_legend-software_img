f09646d174d616b72f17111b374467162a1025ca
Oliver Schulz <oschulz@mpp.mpg.de>
2020-02-19 15:25:31 +0100

    Fix/update pygama install script

--- provisioning/install-sw-scripts/pygama-setup.sh
+++ provisioning/install-sw-scripts/pygama-setup.sh
@@ -7,26 +7,27 @@ DEFAULT_BUILD_OPTS=""
 
 
 pkg_installed_check() {
-    test -f "${INSTALL_PREFIX}/bin/gears"
-}
-
-python_sitepkg_dir() {
-    python_version=`python -V 2>&1 | head -n1 | \sed 's/[^0-9]*\([0-9][0-9]*\.[0-9][0-9]*\).*/\1/'`
-    echo "${INSTALL_PREFIX}/lib/python${python_version}/site-packages"
+    test -f "${INSTALL_PREFIX}/lib/python3.7/site-packages/site.py"
 }
 
 
 pkg_install() {
     GITHUB_USER=`echo "${PACKAGE_VERSION}" | cut -d '/' -f 1`
     GIT_BRANCH=`echo "${PACKAGE_VERSION}" | cut -d '/' -f 2`
-    git clone "https://github.com/${GITHUB_USER}/pygama" pygama
+    git clone --recursive "https://github.com/${GITHUB_USER}/pygama" pygama
+    (cd pygama && git checkout "${GIT_BRANCH}" && git submodule update)
 
+    export PYTHONUSERBASE="${INSTALL_PREFIX}"
+
+    # pygama needs tinydb (missing in pygama deps?)
+    python3 -m pip install --user tinydb
+
+    # Just to install pygama deps:
+    python3 -m pip install --user -e pygama
+
+    # Install pygama to INSTALL_PREFIX:
     cd pygama
-    git checkout "${GIT_BRANCH}"
-
-    mkdir -p `python_sitepkg_dir`
     python setup.py install --prefix="${INSTALL_PREFIX}"
-    rm -rf "${INSTALL_PREFIX}/bin"
 }
 
 
