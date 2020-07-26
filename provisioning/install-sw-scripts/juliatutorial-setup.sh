19a327e834b520671fd4088572cb487399c5155f
Oliver Schulz <oschulz@mpp.mpg.de>
2020-07-26 14:00:02 +0200

    Build LEGEND Julia system image

--- provisioning/install-sw-scripts/juliatutorial-setup.sh
+++ provisioning/install-sw-scripts/juliatutorial-setup.sh
@@ -17,17 +17,32 @@ pkg_install() {
     git clone "https://github.com/${GITHUB_USER}/legend-julia-tutorial" "${INSTALL_PREFIX}"
 
     export JULIA_DEPOT_PATH="/opt/julia/local/share/julia"
+    export JULIA_PROJECT=$(dirname `julia -e 'import Pkg; println(Pkg.project().path)'`)
+ 
     export JUPYTER_DATA_DIR="/opt/anaconda3/share/jupyter"
 
-    julia -e 'using Pkg; pkg"add IJulia; precompile"'
+    DEFAULT_NUM_THREADS=`lscpu -p | grep '^[0-9]\+,[0-9]\+,[0-9]\+,0,' | cut -d ',' -f 2 | sort | uniq | wc -l`
+    export JULIA_NUM_THREADS="${DEFAULT_NUM_THREADS}"
+    export OPENBLAS_NUM_THREADS="${DEFAULT_NUM_THREADS}"
+    export OMP_NUM_THREADS="${DEFAULT_NUM_THREADS}"
 
-    julia --project="${INSTALL_PREFIX}" -e 'using Pkg; pkg"instantiate; precompile"'
+    julia -e 'using Pkg; Pkg.update()'
+    cp -a "${INSTALL_PREFIX}"/{Project.toml,Manifest.toml} "${JULIA_PROJECT}"
+    julia -e 'import Pkg; Pkg.instantiate()'
+    julia -e 'using Pkg; Pkg.add("IJulia"; preserve=Pkg.PRESERVE_ALL); Pkg.build("IJulia")'
+    julia -e 'import Pkg; Pkg.precompile()'
 
-    mkdir tmpprj
-    cp -a "${INSTALL_PREFIX}/Project.toml" "${INSTALL_PREFIX}/Manifest.toml" tmpprj
-    julia --project=tmpprj -e 'using Pkg; pkg"update; precompile"'
+    DEFAULT_SYSIMG=`julia -e 'import Libdl; println(abspath(Sys.BINDIR, "..", "lib", "julia", "sys." * Libdl.dlext))'`
 
+    (cd "${INSTALL_PREFIX}" && julia "build_sysimage.jl")
+    mv "${DEFAULT_SYSIMG}" "${DEFAULT_SYSIMG}.backup"
+    mv "${INSTALL_PREFIX}/julia_sysimage.so" "${DEFAULT_SYSIMG}"
+
+    rm -rf /opt/julia/local/share/julia/logs
     chmod -R go+rX  "$JULIA_DEPOT_PATH"
+
+    # Test:
+    julia -e 'using LegendDataTypes'
 }
 
 
